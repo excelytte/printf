a148e6f771b49bbd04e01c8aa36744a995f221ea
Akinee Essential <akineeessential@gmail.com>
2023-09-19 19:33:56 +0000

    main.h added

---
+++ main.h
@@ -0,0 +1,11 @@
+/**
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
+typedef struct fmt fmt_t;
+
+int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
